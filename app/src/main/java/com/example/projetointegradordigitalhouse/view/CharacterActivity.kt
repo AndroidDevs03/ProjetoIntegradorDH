1f11a8914ebd82ec8ec6915fbb87b488200f5332
Jônatas Araujo <jonatasaraujo@gmail.com>
2020-11-20 11:00:24 -0300

    migrando CharacterActivity para pacote MVVM

--- app/src/main/java/com/example/projetointegradordigitalhouse/view/CharacterActivity.kt
+++ app/src/main/java/com/example/projetointegradordigitalhouse/view/CharacterActivity.kt
@@ -1,10 +1,11 @@
-package com.example.projetointegradordigitalhouse
+package com.example.projetointegradordigitalhouse.view
 
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
+import com.example.projetointegradordigitalhouse.R
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.synnapps.carouselview.CarouselView
 
