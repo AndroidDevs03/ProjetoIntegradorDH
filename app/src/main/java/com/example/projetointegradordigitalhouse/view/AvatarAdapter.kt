aa9663be65737b9e964b467e839448240cd8df71
lubvalentino <lubvalentino@yahoo.com.br>
2021-02-14 17:49:08 -0300

    foto salva

--- app/src/main/java/com/example/projetointegradordigitalhouse/view/AvatarAdapter.kt
+++ app/src/main/java/com/example/projetointegradordigitalhouse/view/AvatarAdapter.kt
@@ -11,12 +11,18 @@ import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.example.projetointegradordigitalhouse.R
+import com.google.firebase.auth.FirebaseUser
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.firestore.ktx.firestore
+import com.google.firebase.ktx.Firebase
 import de.hdodenhof.circleimageview.CircleImageView
 
 class AvatarAdapter(private val listAvatar: List<Int>,
         private val mcontext: Context) : RecyclerView.Adapter<AvatarAdapter.AvatarViewHolder>() {
 
     private lateinit var popUpWindow : PopUpWindow
+
+    private val firebaseAuth by lazy{ Firebase.auth }
 
     override fun onCreateViewHolder(
         parent: ViewGroup,
@@ -29,6 +35,7 @@ class AvatarAdapter(private val listAvatar: List<Int>,
     override fun onBindViewHolder(holder: AvatarViewHolder, position: Int) {
 
         holder.itemView.setOnClickListener {
+            holder.addImage(firebaseAuth.currentUser, position)
             mcontext as PopUpWindow
             mcontext.finish()
         }
@@ -40,8 +47,25 @@ class AvatarAdapter(private val listAvatar: List<Int>,
         return listAvatar.size
     }
 
+
     class AvatarViewHolder (itemView: View):RecyclerView.ViewHolder(itemView) {
+        private val firebaseFirestore by lazy {
+            Firebase.firestore
+        }
+
         val image = itemView.findViewById<CircleImageView>(R.id.civAvatar)
+
+        fun addImage(firebaseUser: FirebaseUser?, position:Int){
+            firebaseUser?.uid?.let {
+                firebaseFirestore.collection("users").document(it).update("avatar_id",position)
+                    .addOnSuccessListener {
+                        Toast.makeText(itemView.context, "Foto salva", Toast.LENGTH_LONG).show()
+                    }
+                    .addOnFailureListener {
+                        Toast.makeText(itemView.context, "Não salvou", Toast.LENGTH_LONG).show()
+                    }
+            }
+        }
     }
 
 }
